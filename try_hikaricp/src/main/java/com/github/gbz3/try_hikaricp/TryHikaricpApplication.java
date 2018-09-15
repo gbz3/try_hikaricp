ddcbe8e2774724dd5abd2bf6325f6eb87a69148b
gbz3 <gebizo3@gmail.com>
2018-09-15 10:03:20 +0900

    init

---
+++ try_hikaricp/src/main/java/com/github/gbz3/try_hikaricp/TryHikaricpApplication.java
@@ -0,0 +1,12 @@
+package com.github.gbz3.try_hikaricp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryHikaricpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryHikaricpApplication.class, args);
+	}
+}
---
+++ try_hikaricp/src/test/java/com/github/gbz3/try_hikaricp/TryHikaricpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.gbz3.try_hikaricp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TryHikaricpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
